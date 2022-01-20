ae06df30a6ccf7bee9d044f5e1bc325fd27b6632
bridgetp24 <bridgetpeterkin@gmail.com>
2022-01-20 13:11:49 -0600

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Bridget Peterkin on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
